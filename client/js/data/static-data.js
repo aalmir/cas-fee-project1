0eac535f38b66c1a080edd441938d36541b3edf3
Urs van Binsbergen <urs.van.binsbergen@hsr.ch>
2019-06-23 11:33:20 +0200

    reduced sample data

--- client/js/data/static-data.js
+++ client/js/data/static-data.js
@@ -2,31 +2,20 @@ export class StaticData {
     static getSampleData() {
         return [
             {
-                "id": 1,
                 "title": "Vanille-Glacé",
-                "description": "",
                 "priority": 1,
                 "dueDate": new Date(2019, 7, 7),
-                "createdDate": new Date(2019, 5, 1),
-                "done": false
             },
             {
-                "id": 2,
                 "title": "Schokoladensauce",
-                "description": "",
                 "priority": 2,
                 "dueDate": null,
-                "createdDate": new Date(2019, 5, 2),
-                "done": false
             },
             {
-                "id": 3,
                 "title": "Salat",
-                "description": "",
+                "description": "Lorem ipsum dolor\nSit amet",
                 "priority": 3,
                 "dueDate": new Date(2019, 8, 2),
-                "createdDate": new Date(2019, 5, 2),
-                "done": false
             }
         ];
     }
