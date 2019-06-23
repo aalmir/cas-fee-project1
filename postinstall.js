b1664fcf64fa879e01c8f58165bbcde6bfab695e
Urs van Binsbergen <urs.van.binsbergen@hsr.ch>
2019-06-23 20:05:58 +0200

    create libs-dir when missing

--- postinstall.js
+++ postinstall.js
@@ -5,6 +5,10 @@ const path = require('path');
 const sourceDir = path.join(__dirname, 'node_modules')
 const targetDir = path.join(__dirname, 'client', 'libs');
 
+if (!fs.existsSync(targetDir)){
+    fs.mkdirSync(targetDir);
+}
+
 fs.copyFileSync(
     path.join(sourceDir, 'handlebars', 'dist', 'handlebars.min.js'), 
     path.join(targetDir, 'handlebars.min.js')
